9493b25d22798065a8d1e1bb388ab294856bc46b
Ahmed Uz Zaman <44180223+ahmed8494@users.noreply.github.com>
2021-09-03 14:59:29 -0500

    2

--- dice.js
+++ dice.js
@@ -19,6 +19,7 @@ function showResetButton(){
 
 rollBtn.addEventListener("click", function(){
     const randomNumber = Math.ceil(Math.random() * 6)
+    console.log("clicked")
     
     if (player1Turn){
         player1Score += randomNumber
@@ -26,12 +27,14 @@ rollBtn.addEventListener("click", function(){
         player1Dice.textContent = randomNumber
         player1Dice.classList.remove("active")
         player2Dice.classList.add("active")
+        message.textContent = "Player 2 Turn"
     } else {
         player2Score += randomNumber
         player2Scoreboard.textContent = player2Score
         player2Dice.textContent = randomNumber
         player2Dice.classList.remove("active")
         player1Dice.classList.add("active")
+        message.textContent = "Player 1 Turn"
     }
 
     if (player1Score>=20){
